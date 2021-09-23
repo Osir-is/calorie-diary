ab8230ef681f211c66282da0406d07f09dd67cc9
Osir-is <78626961+Osir-is@users.noreply.github.com>
2021-09-23 18:44:17 +0100

    created a data structure and all the public method

--- assets/script.js
+++ assets/script.js
@@ -65,3 +65,73 @@ const mealItemCntrl = (function () {
     }
   }
   const id = genID();
+
+// Data Structure / State
+const data = {
+    items: StorageCtrl.getItemFromStorage(),
+    currentItem: null,
+    totalCalories: 0,
+  };
+    //public methods
+    return {
+        getItems: function () {
+          return data.items;
+        },
+        logData: function () {
+          return data;
+        },
+        addItem: function (name, calories) {
+          const newItem = new Item(name, parseInt(calories));
+          data.items.push(newItem);
+          return newItem;
+        },
+        getTotCalories: function () {
+          let cal = 0;
+          data.items.forEach((item) => {
+            cal += item.calories;
+          });
+          data.totalCalories = cal;
+          return data.totalCalories;
+        },
+        getItemByID: function (id) {
+          let found = null;
+          data.items.forEach((item) => {
+            if (item.id === id) {
+              found = item;
+            }
+          });
+          return found;
+        },
+        updateItemByID: function (id, name, calories) {
+          let updatedItem = null;
+          data.items.forEach((item) => {
+            if (item.id === id) {
+              item.name = name;
+              item.calories = parseInt(calories);
+              updatedItem = item;
+            }
+          });
+          return updatedItem;
+        },
+        setCurrentItem: function (item) {
+          data.currentItem = item;
+        },
+        getCurrentItem: function () {
+          return data.currentItem;
+        },
+        itemToBeDeleted: function (id) {
+          //Get ids;
+          const ids = data.items.map((item) => {
+            return item.id;
+          });
+          const index = ids.indexOf(id);
+    
+          //Remove itme
+          data.items.splice(index, 1);
+        },
+        clearAllItems: function () {
+          data.items = [];
+        },
+      };
+    })();
+    
